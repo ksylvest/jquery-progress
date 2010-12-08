2e99461e47b562a0a5dc3c15a64769c84e695c6b
Kevin Sylvestre <kevin@ksylvest.com>
2010-12-08 02:54:49 -0500

    initial import

---
+++ javascripts/jquery.progress.js
@@ -0,0 +1,27 @@
+/*!
+ * jQuery Progress
+ *
+ * Copyright 2010 Kevin Sylvestre
+ */
+
+(function ($) {
+
+  $.fn.progress = function(options) {
+
+    var settings = {
+      inner    : '.inner',
+      outer    : '.outer',
+      percent  : 0.0,
+      duration : 400,
+      easing   : 'swing',
+    };
+    
+    if (options) ($.extend(settings, options));
+    
+    $inner = $(this).find(settings.inner);
+    $outer = $(this).find(settings.outer);
+     
+    $inner.stop().animate({ width: (settings.percent * 100) + "%" }, settings.duration, settings.easing);
+  };
+  
+}) (jQuery);
